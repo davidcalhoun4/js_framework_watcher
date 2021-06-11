29c8dbb8c280cc94028e2a8bdf675ca72b1d15c3
david <calhoun5321@gmail.com>
2021-06-11 14:58:45 -0700

    adds highcharts library, sets up bargraph in home.vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+// import axios from "axios";
+import HighchartsVue from "highcharts-vue";
+
+Vue.use(HighchartsVue);
 
 Vue.config.productionTip = false;
 
